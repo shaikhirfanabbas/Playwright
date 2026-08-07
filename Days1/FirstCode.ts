debcffe127469ae91260cd58947d6b0af2e6a64b
shaikhirfanabbas <shaikhirfanabbas93@gmail.com>
2026-08-07 20:02:01 +0530

    bug fix

--- Days1/FirstCode.ts
+++ Days1/FirstCode.ts
@@ -2,4 +2,6 @@ console.log("Hello world");
 let num=10;
 console.log(num);
 console.log("Hello");
-console.log("Hello world test");
+console.log("Hello world test")
+console.log("Hello world test")
+console.log("Hello world test")
